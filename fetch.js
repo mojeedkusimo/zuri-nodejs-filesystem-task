de562cd3a026744f674e2efef01703536710914f
MOJEED KUSIMO <mkusimo90@gmail.com>
2021-04-19 22:09:07 +0100

    Task completed

--- fetch.js
+++ fetch.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+
 const options = {
     hostname: 'jsonplaceholder.typicode.com',
     path: '/posts',
@@ -8,9 +10,25 @@ const options = {
 const req = http.request(options, res => {
     console.log(res);
 
-    res.on('data', d => {
-        process.stdout.write(d);
-    });
+    // res.on('data', d => {
+    //     // process.stdout.write(d);
+    //     fs.writeFile('/result/posts.json', d, err => {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //         else {
+    //             console.log('Result of api request successfully written into posts.json');
+    //         }
+    //     })
+    // });
+    try {
+        res.on('data', d => {
+            fs.writeFileSync('/home/mojeedkusimo/Desktop/projects/zuri-nodejs-filesystem-task/result/posts.json', d);
+        })
+    }
+    catch (err) {
+        console.error(err);
+    }
 });
 
 req.on('error', err => {
